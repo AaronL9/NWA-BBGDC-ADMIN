0bbe3439027cd8441148a82d05b7d86b42915ba3
Aaron Lomibao <aaron.lomibao09@gmail.com>
2024-01-25 10:37:16 +0800

    tab and track all location logic done

--- src/pages/patrollers/Tabs/AllLocation.jsx
+++ src/pages/patrollers/Tabs/AllLocation.jsx
@@ -1,10 +1,45 @@
-import React from "react";
-import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
+import { db } from "../../../config/firebase";
 
-function AllLocation(props) {
-  return <div>AllLocation</div>;
+export default function AllLocation() {
+  const [patrollersLocation, setPatrollersLocation] = useState([]);
+  useEffect(() => {
+    const fetchPatrollersLocation = async () => {
+      const querySnapshot = await getDocs(collection(db, "patrollers"));
+      const locations = querySnapshot.docs
+        .filter((doc) => {
+          const location = doc.data()?.patrollerLocation;
+          if (!location) return false;
+          return true;
+        })
+        .map((doc) => doc.data().patrollerLocation);
+
+      setPatrollersLocation(locations);
+    };
+    fetchPatrollersLocation();
+  }, []);
+
+  return (
+    <div className="report__map">
+      <APIProvider apiKey={import.meta.env.VITE_FIREBASE_API_KEY}>
+        <Map
+          zoom={14}
+          center={{ lat: 16.071917235901985, lng: 120.3400965898522 }}
+          gestureHandling={"cooperative"}
+        >
+          {patrollersLocation.map((coords, index) => (
+            <Marker key={index} position={coords} />
+          ))}
+          <Marker
+            position={{ lat: 16.069587275537636, lng: 120.33473217207258 }}
+          />
+          <Marker
+            position={{ lat: 16.076608097634953, lng: 120.34106191076333 }}
+          />
+        </Map>
+      </APIProvider>
+    </div>
+  );
 }
-
-AllLocation.propTypes = {};
-
-export default AllLocation;
