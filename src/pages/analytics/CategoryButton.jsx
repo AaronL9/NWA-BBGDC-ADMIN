cbdc3f79ecb0be7759bd94130bab9d0fb31b0f47
Aaron Lomibao <aaron.lomibao09@gmail.com>
2024-03-14 21:30:44 +0800

    store static data in database

--- src/pages/analytics/CategoryButton.jsx
+++ src/pages/analytics/CategoryButton.jsx
@@ -4,43 +4,28 @@ import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import PropTypes from "prop-types";
-
-const bonuanArea = [
-  "all",
-  "alimango",
-  "alimasag",
-  "apahap",
-  "ayungin",
-  "bangus avenue",
-  "baracuda",
-  "bisugo",
-  "bonito",
-  "calamiong",
-  "dalagang bukid",
-  "dasmarinas",
-  "don marcelo i. balolong sr. avenue",
-  "don maximo",
-  "eastern county",
-  "las vegas",
-  "maramba banker's village",
-  "paras",
-  "perrenians",
-  "sabangan",
-  "san marino place",
-  "sugpo",
-  "tanguegue",
-  "tondaligan",
-  "tuna",
-];
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../../config/firebase";
 
 export default function CategoryButton({ setCategory }) {
-  const [area, setArea] = React.useState(bonuanArea[0]);
+  const [area, setArea] = React.useState("");
+  const [bonuanArea, setBonuanArea] = React.useState([]);
 
   const handleChange = (event) => {
     const value = event.target.value;
     setArea(value);
     setCategory(value.split(" ").join("_").toLowerCase());
   };
+
+  React.useEffect(() => {
+    const getData = async () => {
+      const area = await getDoc(doc(db, "static_data", "area"));
+      const data = area.data().options;
+      setBonuanArea(data);
+      setArea(data[0]);
+    };
+    getData();
+  }, []);
 
   return (
     <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
